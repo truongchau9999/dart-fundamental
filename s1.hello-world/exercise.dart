cf2bcc85f14077b1b6bc99909d1ad74063a59e78
truongchau9999 <chaunhattruong9999@gmail.com>
2021-12-08 02:56:04 +0700

    Learn data type and do excercise

---
+++ s1.hello-world/exercise.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  double temperature = 20;
+  int value = 2;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  // Output:
+  // The temperature is 20C
+  // 2 plus 2 makes 4
+  // I like pizza and pasta
+
+  print("-----------------------");
+
+  print("The temperature is ${temperature}C");
+  print("$value plus $value makes ${value + value}");
+  print("I like $pizza and $pasta");
+}
--- s1.hello-world/main.dart
+++ s1.hello-world/main.dart
@@ -1,3 +1,63 @@
 main(List<String> args) {
   print("Hello World");
+
+  print("-----------------------------");
+
+  //String type
+  String name = "Truong";
+  print(name);
+
+  //integer type
+  int age = 25;
+  print(age);
+
+  //double type
+  double height = 1.67;
+  print(height);
+
+  //boolean type
+  bool male = true;
+  print(male);
+
+  print("-----------------------------");
+
+  //concatenation
+  print("Ten " +
+      name +
+      ", " +
+      age.toString() +
+      " tuoi, " +
+      "cao " +
+      height.toString() +
+      " met, la nam " +
+      male.toString());
+
+  //interpolation
+  print("Ten $name, $age tuoi, cao $height met, la nam $male");
+
+  print("-----------------------------");
+
+  //String escaping
+  print("Today I'm feeling great");
+  print('Today I\'m feeling great');
+  print("\\");
+  print("\$");
+  print("C:\\Windows\\systerm32");
+
+  print("-----------------------------");
+  //Raw String
+  print(r"C:\Windows\systerm32");
+
+  print("-----------------------------");
+
+  //multi-line String
+  print('This is a short sentence.\n'
+      'This is a longer sentence , I dare say.\n'
+      'This is an even longer sentence, which will not fit inside a single line.');
+
+  print('''
+This is a short sentence.
+This is a longer sentence , I dare say.
+This is an even longer sentence, which will not fit inside a single line.
+  ''');
 }
